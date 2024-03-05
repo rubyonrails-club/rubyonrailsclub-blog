250ae3183b672612f2d3cc01ed5d5cc64015750b
Jackson Pires <jackson.pires@gmail.com>
2024-03-05 20:25:45 -0300

    adicionando layout administrativo

--- app/controllers/administrate/articles_controller.rb
+++ app/controllers/administrate/articles_controller.rb
@@ -5,6 +5,8 @@ module Administrate
     before_action :authenticate_admin!
     before_action :set_article, only: [:show, :edit, :update, :destroy, :destroy_cover_image]
 
+    layout "administrate"
+
     # GET /articles or /articles.json
     def index
       @articles = Article.all
