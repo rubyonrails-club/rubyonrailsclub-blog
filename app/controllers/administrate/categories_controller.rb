6503d6660cf702ad27905207b18892c2c3485903
Jackson Pires <jackson.pires@gmail.com>
2024-04-21 18:13:14 -0300

    adiciona associacao (has_many)

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -3,7 +3,6 @@
 module Administrate
   class CategoriesController < AdministrateController
     before_action :set_category, only: [:show, :edit, :update, :destroy]
-    # before_action :set_categories, only: [:new, :edit, :show]
 
     # GET /categories or /categories.json
     def index
@@ -56,26 +55,14 @@ module Administrate
       @category.destroy!
 
       respond_to do |format|
-        format.html { redirect_to(administrate_categorys_url, notice: "Artigo apagado com sucesso!") }
+        format.html { redirect_to(administrate_categorys_url, notice: "Categoria apagada com sucesso!") }
         format.json { head(:no_content) }
       end
     end
 
-  #   def destroy_cover_image
-  #     @category.cover_image.purge
+    private
 
-  #     respond_to do |format|
-  #       format.turbo_stream { render(turbo_stream: turbo_stream.remove(@category)) }
-  #     end
-  #   end
-
-  #   private
-
-  #   def set_categories
-  #     @categories = Category.all
-  #   end
-
-  #   # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
     end
