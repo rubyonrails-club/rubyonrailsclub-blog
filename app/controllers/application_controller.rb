50f3b6945ab8bc1b16465fc32945b51a1700feef
Jackson Pires <jackson.pires@gmail.com>
2024-05-06 21:19:50 -0300

    finalizando o rodapé

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_footer_data
+
+  def set_footer_data
+    return if params[:controller].split("/").first == "administrate"
+
+    @footer_categories_1 = Category.all.order(created_at: :asc).limit(4)
+    @footer_categories_2 = Category.all.order(created_at: :desc).limit(4)
+    @footer_articles = Article.all.sample(4)
+  end
 end
