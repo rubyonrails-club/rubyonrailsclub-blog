02e3446c821cba0408e360dc8eb680f8294d70c0
Jackson Pires <jackson.pires@gmail.com>
2024-05-14 22:14:16 -0300

    redirecionando o usuário após login e logout no Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_footer_data
+  before_action :store_user_location!, if: :storable_location?
 
   def set_footer_data
     return if params[:controller].split("/").first == "administrate"
@@ -10,4 +11,32 @@ class ApplicationController < ActionController::Base
     @footer_categories_2 = Category.all.order(created_at: :desc).limit(4)
     @footer_articles = Article.all.sample(4)
   end
+
+  private
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+    #    infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    # - The request is not a Turbo Frame request ([turbo-rails](https://github.com/hotwired/turbo-rails/blob/main/app/controllers/turbo/frames/frame_request.rb))
+    def storable_location?
+      request.get? &&
+        is_navigational_format? &&
+        !devise_controller? &&
+        !request.xhr? &&
+        !turbo_frame_request?
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope)
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+      request.referrer if resource_or_scope == :user
+    end
 end
