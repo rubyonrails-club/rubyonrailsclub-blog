0609dccec90d18901dd563286089fea61295a0a3
Jackson Pires <jackson.pires@gmail.com>
2024-05-20 22:06:04 -0300

    ajustando pagina inicia, logoff do admin e usando markdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,5 +39,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     request.referer if resource_or_scope == :user
+    super
   end
 end
--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -4,4 +4,8 @@ module ArticleHelper
   def cover_image_fallback(article, size = :thumb)
     article.cover_image.attached? ? article.cover_image.variant(size) : "posts/post1.jpg"
   end
+
+  def markdown(md)
+    Markdown.new(md).to_html.html_safe
+  end
 end
