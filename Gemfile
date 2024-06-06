fc02b9f8e33a76c655a6158b180fb28e801c9881
Jackson Pires <jackson.pires@gmail.com>
2024-06-06 11:27:00 -0300

    trabalhando com pdfs (ferrum + rmagick)

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem "image_processing", "~> 1.2"
 gem "sassc-rails"
 gem "redcarpet"
 gem "prawn-rails"
+gem "ferrum"
+gem "rmagick"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: [:mri, :windows]
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,22 @@ class ArticlesController < ApplicationController
     @article = Article.includes(:category, :author, comments: :user).friendly.find(params[:id])
     @other_articles = Article.all.sample(3)
     @comments = comments_sorted_by
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        browser = Ferrum::Browser.new(timeout: 15, window_size: [1920, 1080])
+        browser.go_to(article_url(@article))
+        browser.page.network.wait_for_idle
+        browser.screenshot(path: "public/#{@article.id}.png", full: true)
+        browser.quit
+
+        png = Magick::ImageList.new(Rails.root.join("public/#{@article.id}.png"))
+        png.write(Rails.root.join("public/#{@article.id}.pdf"))
+
+        send_file("public/#{@article.id}.pdf", type: "application/pdf", disposition: "inline")
+      end
+    end
   end
 
   private
